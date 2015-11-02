5588e6a6a8482a9d9b14691cbf95591b65f39fde
James Turnbull <james@lovedthanlost.net>
2015-11-02 21:40:03 +0100

    Added a Statsd timer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,9 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    STATSD.time("find_user") do
+      @user = User.find(params[:id])
+    end
     unless current_user.admin?
       unless @user == current_user
         redirect_to :back, :alert => "Access denied."
