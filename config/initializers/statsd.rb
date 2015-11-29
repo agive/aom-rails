347e313ee9e3593f2d45fba93679dd9cf22bc797
agive <agive196641@gmail.com>
2015-11-29 18:01:33 +0000

    * Statsd support
    Added base statsd support
    Added statsd counter for user creation and deletion
    Added statsd timer

--- config/initializers/statsd.rb
+++ config/initializers/statsd.rb
@@ -1,6 +1,4 @@
 # Configure the statsd client
 
-ip_or_hostname = Resolv.getaddress("statsd.example.com") rescue "statsd.example.com"
-STATSD = Statsd.new(ip_or_hostname, 8125)
-STATSD.namespace = "aom-rails"
-
+STATSD = Statsd.new("localhost", 8125)
+STATSD.namespace = "aom-rails."
